a2ca07eb4f614fe49098727cb6573f53687abb70
Gleb <gudleifre@yandex.ru>
2026-04-23 15:15:26 +0300

    Initial Commit

---
+++ GGSBreath/GGSBreathApp.swift
@@ -0,0 +1,17 @@
+//
+//  GGSBreathApp.swift
+//  GGSBreath
+//
+//  Created by Глеб on 23.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct GGSBreathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
